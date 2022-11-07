03f8327fa5989a0241ffc30d47f4ae2239872e00
Shaurya <smilingshaurya@gmail.com>
2022-11-08 01:43:58 +0530

    Remove commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side JS Working");
-
 const form = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
